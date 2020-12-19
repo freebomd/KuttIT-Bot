d7c99a55cb3fefb054e98cf56f0d60e4bd04a976
freebomd <76235334+freebomd@users.noreply.github.com>
2020-12-20 00:22:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-    kuttit_api_key: process.env.kuttit_api_key || "",
-    bot_api_key: process.env.bot_api_key || ""
+    kuttit_api_key: process.env.kuttit_api_key || "v7EqFs3ZFdtDXthrf8yWwCCPANDeYU9TStmmVIFE",
+    bot_api_key: process.env.bot_api_key || "1485764380:AAGrKmPS3J7_TSVFFW4aF7r79KsvdGXRW8k"
 };
